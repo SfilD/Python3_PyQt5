adb82167ac16067cf1615d1f989387cfd6faec92
SfilD <pixie.winx.club@gmail.com>
2020-04-02 22:14:24 +0900

    Win10 Desktop Home 02.04.2020 22:14

--- lst_1-1.py
+++ lst_1-1.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 # Проверка установки
 import sys
-
 print(tuple(sys.version_info))
-try:
-    raw_input()     # Python 2
-
-except NameError:
-    input()         # Python 3
---
+++ lst_1-5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Выделение инструкций внутри блока
+
+i = 1
+while i < 11:
+    print(i)
+    i += 1
+print("Конец программы")
